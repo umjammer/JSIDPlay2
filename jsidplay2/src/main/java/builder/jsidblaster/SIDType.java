631224022b3f495298219ac74f90f717432dfb13
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2022-03-26 09:46:38 +0000

    IAE fixed
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3896 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/builder/jsidblaster/SIDType.java
+++ jsidplay2/src/main/java/builder/jsidblaster/SIDType.java
@@ -3,20 +3,10 @@ package builder.jsidblaster;
 import libsidplay.common.ChipModel;
 
 public enum SIDType {
-	NONE(""), MOS6581("6581"), MOS8580("8580");
-
-	private String sidType;
-
-	private SIDType(String sidType) {
-		this.sidType = sidType;
-	}
-
-	public String getSidType() {
-		return sidType;
-	}
+	NONE, MOS6581, MOS8580;
 
 	public ChipModel asChipModel() {
-		return ChipModel.valueOf(sidType);
+		return ChipModel.valueOf(name());
 	}
 
 	public static SIDType to(sidblaster.SIDType hardSID_GetSIDType) {
