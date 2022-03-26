042df556d42c4e23863d3614a19ba029aa974c0c
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2022-03-26 09:45:11 +0000

    simplification
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@3895 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- sidblaster-usb-driver-interface-library-java/sidblaster-usb-driver-interface-library-java/src/main/java/sidblaster/SIDType.java
+++ sidblaster-usb-driver-interface-library-java/sidblaster-usb-driver-interface-library-java/src/main/java/sidblaster/SIDType.java
@@ -1,15 +1,6 @@
 package sidblaster;
 
 public enum SIDType {
-	NONE(""), MOS6581("6581"), MOS8580("8580");
+	NONE, MOS6581, MOS8580;
 
-	private String sidType;
-
-	private SIDType(String sidType) {
-		this.sidType = sidType;
-	}
-
-	public String getSidType() {
-		return sidType;
-	}
 }
