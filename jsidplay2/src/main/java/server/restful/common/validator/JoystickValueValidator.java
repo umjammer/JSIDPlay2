a1c54d01074ef4255c4aac4468a13a62cd0770fe
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2022-06-07 17:19:34 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4102 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/server/restful/common/validator/JoystickValueValidator.java
+++ jsidplay2/src/main/java/server/restful/common/validator/JoystickValueValidator.java
@@ -6,9 +6,13 @@ import com.beust.jcommander.ParameterException;
 public class JoystickValueValidator implements IParameterValidator {
 	@Override
 	public void validate(String name, String value) throws ParameterException {
-		int n = Integer.parseInt(value);
-		if (n < 0 || n > 255) {
-			throw new ParameterException("Invalid " + name + " value, expected 0..255 (found " + value + ")");
+		try {
+			int n = Integer.parseInt(value);
+			if (n < 0 || n > 255) {
+				throw new ParameterException("Invalid " + name + " value, expected 0..255 (found " + value + ")");
+			}
+		} catch (NumberFormatException e) {
+			throw new ParameterException("Parameter " + name + " should be an int (found " + value + ")");
 		}
 	}
 }
