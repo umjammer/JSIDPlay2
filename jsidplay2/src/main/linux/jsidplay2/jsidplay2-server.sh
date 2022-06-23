1088bf33431b26cbb41c5a7e54fb9aca18ce487a
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2022-06-23 20:54:25 +0000

    comments
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4165 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/linux/jsidplay2/jsidplay2-server.sh
+++ jsidplay2/src/main/linux/jsidplay2/jsidplay2-server.sh
@@ -4,7 +4,5 @@ java -server -classpath ./${project.artifactId}-${project.version}.jar server.re
 
 # E.g. MySQL:
 # --whatsSIDDatabaseUrl "jdbc:mysql://127.0.0.1:3306/hvscXX?createDatabaseIfNotExist=true&useUnicode=yes&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true"
-# E.g. video streaming with HLS instead of RTMP:
-# -Djsidplay2.rtmp.wait.for=4000 -D"jsidplay2.rtmp.internal.download.url=http://haendel.ddns.net:90/hls" -D"jsidplay2.rtmp.external.download.url=http://haendel.ddns.net:90/hls"
 # E.g. Monitoring with jvisualvm:
 # -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=192.168.1.150
