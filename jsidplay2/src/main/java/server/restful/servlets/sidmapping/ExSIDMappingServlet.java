6f3559f50041800daee64e5968a3fbf987f10fed
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2022-11-12 02:43:54 +0000

    missing resources added
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4458 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

---
+++ jsidplay2/src/main/java/server/restful/servlets/sidmapping/ExSIDMappingServlet.java
@@ -0,0 +1,105 @@
+package server.restful.servlets.sidmapping;
+
+import static libsidplay.components.pla.PLA.MAX_SIDS;
+import static server.restful.JSIDPlay2Server.CONTEXT_ROOT_SERVLET;
+import static server.restful.common.ContentTypeAndFileExtensions.MIME_TYPE_JSON;
+import static server.restful.common.ContentTypeAndFileExtensions.MIME_TYPE_TEXT;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameters;
+import com.beust.jcommander.ParametersDelegate;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import libsidplay.common.ChipModel;
+import libsidplay.config.IEmulationSection;
+import libsidplay.sidtune.SidTune;
+import server.restful.common.JSIDPlay2Servlet;
+import server.restful.common.parameter.RequestPathServletParameters.FileRequestPathServletParameters;
+import sidplay.ini.IniConfig;
+import ui.entities.config.Configuration;
+
+@SuppressWarnings("serial")
+public class ExSIDMappingServlet extends JSIDPlay2Servlet {
+
+	@Parameters(resourceBundle = "server.restful.servlets.sidmapping.ExSIDMappingServletParameters")
+	public static class ExSIDMappingServletParameters extends FileRequestPathServletParameters {
+
+		@ParametersDelegate
+		private IEmulationSection emulationSection = new IniConfig().getEmulationSection();
+
+	}
+
+	public static final String EXSID_MAPPING_PATH = "/exsid-mapping";
+
+	public ExSIDMappingServlet(Configuration configuration, Properties directoryProperties) {
+		super(configuration, directoryProperties);
+	}
+
+	@Override
+	public String getServletPath() {
+		return CONTEXT_ROOT_SERVLET + EXSID_MAPPING_PATH;
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		super.doGet(request);
+		try {
+			final ExSIDMappingServletParameters servletParameters = new ExSIDMappingServletParameters();
+
+			JCommander commander = parseRequestParameters(request, response, servletParameters, getServletPath());
+
+			final File file = getFile(commander, servletParameters, true);
+			if (file == null) {
+				commander.usage();
+				return;
+			}
+			final IEmulationSection emulationSection = servletParameters.emulationSection;
+
+			SidTune tune = SidTune.load(file);
+
+			Set<ChipModel> alreadyInUse = new HashSet<>();
+			Map<Integer, String> result = new HashMap<>();
+			for (int sidNum = 0; sidNum < MAX_SIDS; sidNum++) {
+				if (SidTune.isSIDUsed(emulationSection, tune, sidNum)) {
+
+					int address = SidTune.getSIDAddress(emulationSection, tune, sidNum);
+
+					ChipModel chipModel = ChipModel.getChipModel(emulationSection, tune, sidNum);
+
+					if (sidNum == 1 && SidTune.isFakeStereoSid(emulationSection, tune, 1)) {
+						continue;
+					}
+					// stereo SIDs with same chipmodel must be forced to use a different device,
+					// therefore:
+					if (sidNum == 1 && isChipNumAlreadyUsed(alreadyInUse, chipModel)) {
+						chipModel = chipModel == ChipModel.MOS6581 ? ChipModel.MOS8580 : ChipModel.MOS6581;
+					}
+					result.put(address, String.valueOf(sidNum));
+					alreadyInUse.add(chipModel);
+				}
+			}
+			setOutput(response, MIME_TYPE_JSON, OBJECT_MAPPER.writer().writeValueAsString(result));
+
+		} catch (Throwable t) {
+			response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+			error(t);
+			setOutput(response, MIME_TYPE_TEXT, t);
+		}
+	}
+
+	private boolean isChipNumAlreadyUsed(Set<ChipModel> alreadyInUse, final ChipModel chipModel) {
+		return alreadyInUse.contains(chipModel);
+	}
+
+}
