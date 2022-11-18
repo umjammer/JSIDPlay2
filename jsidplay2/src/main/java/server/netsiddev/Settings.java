7d327f07cd70e6b8d8b48d722cb6ad5a5be52864
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2022-11-18 20:50:49 +0000

    warning suppressed
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4479 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/server/netsiddev/Settings.java
+++ jsidplay2/src/main/java/server/netsiddev/Settings.java
@@ -61,6 +61,7 @@ public class Settings extends SIDDeviceStage {
 
 	private SIDDeviceSettings settings;
 
+	@SuppressWarnings("unchecked")
 	public Settings() {
 		getContentPane().setLayout(new GridBagLayout());
 		GridBagConstraints gridBagConstants = new GridBagConstraints();
