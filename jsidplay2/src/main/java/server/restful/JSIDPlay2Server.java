3df2fc25dc50731d538c714ab9b3131056d7fcb6
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2023-01-22 23:57:03 +0000

    lifecycle listener added
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4627 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/server/restful/JSIDPlay2Server.java
+++ jsidplay2/src/main/java/server/restful/JSIDPlay2Server.java
@@ -37,6 +37,7 @@ import org.apache.catalina.LifecycleException;
 import org.apache.catalina.LifecycleState;
 import org.apache.catalina.connector.Connector;
 import org.apache.catalina.realm.MemoryRealm;
+import org.apache.catalina.startup.ContextConfig;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.coyote.http11.Http11Nio2Protocol;
 import org.apache.coyote.http2.Http2Protocol;
@@ -391,8 +392,10 @@ public class JSIDPlay2Server {
 	}
 
 	private Context addContext(Tomcat tomcat) {
-		return tomcat.addContext(tomcat.getHost(), CONTEXT_ROOT,
+		Context ctx = tomcat.addContext(tomcat.getHost(), CONTEXT_ROOT,
 				tomcat.getServer().getCatalinaBase().getAbsolutePath());
+		ctx.addLifecycleListener(new ContextConfig());
+		return ctx;
 	}
 
 	private List<JSIDPlay2Servlet> addServlets(Context context) throws InstantiationException, IllegalAccessException,
