7690c3db3dba6cfe1a6b3fea469095be0b8ef9ac
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2023-01-29 09:17:41 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4639 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/linux/jsidplay2/jsidplay2-server.sh
+++ jsidplay2/src/main/linux/jsidplay2/jsidplay2-server.sh
@@ -2,7 +2,13 @@
 pkill -f server.restful.JSIDPlay2Server
 java -server -classpath ./${project.artifactId}-${project.version}.jar server.restful.JSIDPlay2Server "$@"
 
+# E.g. Set language
+# -Duser.language=en
 # E.g. MySQL:
 # --whatsSIDDatabaseUrl "jdbc:mysql://127.0.0.1:3306/hvscXX?createDatabaseIfNotExist=true&useUnicode=yes&characterEncoding=UTF-8&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&autoReconnect=true"
-# E.g. Monitoring with jvisualvm:
+# E.g. RTMP and HLS video support for upload/download videos (for example NGINX server):
+# -Djsidplay2.rtmp.upload.url=rtmp://127.0.0.1/live -Djsidplay2.rtmp.external.download.url=rtmp://haendel.ddns.net/live -Djsidplay2.hls.external.download.url=http://haendel.ddns.net:90/hls
+# E.g. adjust anchor tags in documentation:
+# -Djsidplay2.base.url=https://haendel.ddns.net:8443
+# E.g. Tomcat monitoring with jvisualvm:
 # -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=192.168.1.150
