3e9aed8f229ec1b48def0abec7d88792823293ee
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2023-04-30 02:36:25 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4717 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/server/netsiddev/DetectionThread.java
+++ jsidplay2/src/main/java/server/netsiddev/DetectionThread.java
@@ -11,69 +11,64 @@ public class DetectionThread extends Thread {
 
 	private static final String MAGIC_ID = "SidDevice";
 	private DatagramSocket socket;
-    private byte[] buf = new byte[512];
-    private String localIp;
-    private String systemName;
+	private byte[] buf = new byte[512];
+	private String localIp;
+	private String systemName;
 
-    public DetectionThread(final JSIDDeviceConfig config) {
-        try {
-        	final InetAddress inetAddress = InetAddress.getLocalHost();
-        	localIp = inetAddress.getHostAddress();
+	public DetectionThread(final JSIDDeviceConfig config) {
+		try {
+			final InetAddress inetAddress = InetAddress.getLocalHost();
+			localIp = inetAddress.getHostAddress();
 			systemName = inetAddress.getHostName();
-			
+
 			socket = new DatagramSocket(config.jsiddevice().getPort());
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-    }
+	}
 
-    public void run() {
-    	boolean running = true;
-        
-        final String osName = System.getProperty("os.name");
-        final String osArch = is64Bit() ? "64-bit" : "32-bit";
+	@Override
+	public void run() {
+		boolean running = true;
 
-        final byte[] responseData = new StringBuilder()
-			.append(MAGIC_ID)
-			.append(",")
-			.append(systemName)
-			.append(",")
-			.append(osName)
-        	.append(" ")
-        	.append(osArch)
-        	.toString().getBytes();
-        
-        final SIDDeviceSettings settings = SIDDeviceSettings.getInstance();
-        
-    	final DatagramPacket incomingPacket = new DatagramPacket(buf, buf.length);
+		final String osName = System.getProperty("os.name");
+		final String osArch = is64Bit() ? "64-bit" : "32-bit";
+
+		final byte[] responseData = new StringBuilder().append(MAGIC_ID).append(",").append(systemName).append(",")
+				.append(osName).append(" ").append(osArch).toString().getBytes();
+
+		final SIDDeviceSettings settings = SIDDeviceSettings.getInstance();
+
+		final DatagramPacket incomingPacket = new DatagramPacket(buf, buf.length);
 		final DatagramPacket responsePacket = new DatagramPacket(responseData, responseData.length);
 
-    	while (running) {
-            try {
+		while (running) {
+			try {
 				socket.receive(incomingPacket);
-	            final String received = new String(incomingPacket.getData(), 0, incomingPacket.getLength());
+				final String received = new String(incomingPacket.getData(), 0, incomingPacket.getLength());
 
-	            if (received.startsWith(MAGIC_ID) && (isLocalIp(incomingPacket) || settings.getAllowExternalConnections())) {
-	                responsePacket.setAddress(incomingPacket.getAddress());
-	                responsePacket.setPort(incomingPacket.getPort());
-    				socket.send(responsePacket);
-	            }
+				if (received.startsWith(MAGIC_ID)
+						&& (isLocalIp(incomingPacket) || settings.getAllowExternalConnections())) {
+					responsePacket.setAddress(incomingPacket.getAddress());
+					responsePacket.setPort(incomingPacket.getPort());
+					socket.send(responsePacket);
+				}
 			} catch (IOException ex) {
 				ex.printStackTrace();
 				running = false;
 			}
-        }
-    	
-        socket.close();
-    }
+		}
+
+		socket.close();
+	}
 
 	private boolean isLocalIp(final DatagramPacket incomingPacket) {
 		return incomingPacket.getAddress().getHostAddress().equals(localIp);
 	}
-    
+
 	private static boolean is64Bit() {
-	    final String arch = System.getProperty("os.arch");
-	    final String wow64Arch = System.getenv("PROCESSOR_ARCHITEW6432");
-	    return arch != null && arch.contains("64") || wow64Arch != null && wow64Arch.contains("64");
+		final String arch = System.getProperty("os.arch");
+		final String wow64Arch = System.getenv("PROCESSOR_ARCHITEW6432");
+		return arch != null && arch.contains("64") || wow64Arch != null && wow64Arch.contains("64");
 	}
 }
