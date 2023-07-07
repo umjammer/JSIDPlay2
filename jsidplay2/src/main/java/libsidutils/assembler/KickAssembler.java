bccc3d7b62556239882b9a0fd4796b051af3546d
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2023-07-07 20:38:11 +0000

    Kickassembler is not thread-safe unfortunately
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4880 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/libsidutils/assembler/KickAssembler.java
+++ jsidplay2/src/main/java/libsidutils/assembler/KickAssembler.java
@@ -28,64 +28,67 @@ public class KickAssembler {
 	 * @return assembly bytes of the ASM resource
 	 */
 	public KickAssemblerResult assemble(String resource, InputStream asm, final Map<String, String> globals) {
-		ByteArrayOutputStream result = new ByteArrayOutputStream();
+		synchronized (getClass()) {
+			ByteArrayOutputStream result = new ByteArrayOutputStream();
 
-		final EvaluationState state = new EvaluationState();
-		try {
-			state.outputMgr = (a, b) -> result;
+			final EvaluationState state = new EvaluationState();
+			try {
+
+				state.outputMgr = (a, b) -> result;
 			state.c64OutputMgr.inputfileWithoutExt = resource;
 			state.parameters.outputfile = resource + ".bin";
 
-			HashtableValue globalValues = new HashtableValue().addStringValues(globals);
-			globalValues.lock((SourceRange) null);
-			state.namespaceMgr.getSystemNamespace().getScope()
-					.defineErrorIfExist("cmdLineVars", x -> new ConstantValueHolder(globalValues), state,
-							"ERROR! cmdLineVars is already defined", (SourceRange) null)
-					.setStatus(SymbolStatus.defined);
+				HashtableValue globalValues = new HashtableValue().addStringValues(globals);
+				globalValues.lock((SourceRange) null);
+				state.namespaceMgr.getSystemNamespace().getScope()
+						.defineErrorIfExist("cmdLineVars", x -> new ConstantValueHolder(globalValues), state,
+								"ERROR! cmdLineVars is already defined", (SourceRange) null)
+						.setStatus(SymbolStatus.defined);
 
-			state.prepareNewPass();
-			AsmNode currentAsmNode = new ScopeAndSymbolPageNode(
-					new AsmNodePair(new AsmNodeList(new ArrayList<>()).executeMetaRegistrations(state),
-							new NamespaceNode(
-									AssemblerToolbox.loadAndLexOrError(asm, resource, state, (SourceRange) null),
-									state.namespaceMgr.getRootNamespace()).executeMetaRegistrations(state)),
-					state.namespaceMgr.getSystemNamespace().getScope()).executePrepass(state);
-			this.printErrorsAndTerminate(state);
-
-			do {
 				state.prepareNewPass();
-				currentAsmNode = currentAsmNode.executePass(state);
-				state.segmentMgr.postPassExecution();
+				AsmNode currentAsmNode = new ScopeAndSymbolPageNode(
+						new AsmNodePair(new AsmNodeList(new ArrayList<>()).executeMetaRegistrations(state),
+								new NamespaceNode(
+										AssemblerToolbox.loadAndLexOrError(asm, resource, state, (SourceRange) null),
+										state.namespaceMgr.getRootNamespace()).executeMetaRegistrations(state)),
+						state.namespaceMgr.getSystemNamespace().getScope()).executePrepass(state);
 				this.printErrorsAndTerminate(state);
-				if (!state.getMadeMetaProgress() && !currentAsmNode.isFinished()) {
-					state.prepareNewPass();
-					state.setFailOnInvalidValue(true);
-					currentAsmNode.executePass(state);
-					throw new AsmErrorException(
-							"Made no progress and can't solve the program.. You should have gotten an error. Contact the author!",
-							(SourceRange) null);
-				}
-			} while (!currentAsmNode.isFinished());
-			MainOutputReciever mainOutputReceiver = new MainOutputReciever(state.outputMgr, state.log);
-			currentAsmNode.deliverOutput(mainOutputReceiver);
-			mainOutputReceiver.finish();
-			state.segmentMgr.postPassesExecution();
-			this.printErrorsAndTerminate(state);
-			state.c64OutputMgr.postPassExecution();
-			this.printErrorsAndTerminate(state);
-			state.segmentMgr.doOutputAfterPasses();
-			this.printErrorsAndTerminate(state);
 
-			Map<String, Integer> resolvedSymbols = state.scopeMgr.getResolvedSymbols().stream()
-					.collect(Collectors.toMap(res -> res.name, res -> res.address, (entry1, entry2) -> entry2));
-			return new KickAssemblerResult(result.toByteArray(), resolvedSymbols);
-		} catch (AsmErrorException e) {
-			IDiagnostic asmError = e.getError();
-			asmError.setCallStack(state.callStack);
-			System.err.println(OneLineErrorPrinter.instance.printError(asmError, state));
-			throw new AsmErrorException(asmError, false);
-		} catch (Exception e) {
-			throw new RuntimeException("Internal Error!", e);
+				do {
+					state.prepareNewPass();
+					currentAsmNode = currentAsmNode.executePass(state);
+					state.segmentMgr.postPassExecution();
+					this.printErrorsAndTerminate(state);
+					if (!state.getMadeMetaProgress() && !currentAsmNode.isFinished()) {
+						state.prepareNewPass();
+						state.setFailOnInvalidValue(true);
+						currentAsmNode.executePass(state);
+						throw new AsmErrorException(
+								"Made no progress and can't solve the program.. You should have gotten an error. Contact the author!",
+								(SourceRange) null);
+					}
+				} while (!currentAsmNode.isFinished());
+				MainOutputReciever mainOutputReceiver = new MainOutputReciever(state.outputMgr, state.log);
+				currentAsmNode.deliverOutput(mainOutputReceiver);
+				mainOutputReceiver.finish();
+				state.segmentMgr.postPassesExecution();
+				this.printErrorsAndTerminate(state);
+				state.c64OutputMgr.postPassExecution();
+				this.printErrorsAndTerminate(state);
+				state.segmentMgr.doOutputAfterPasses();
+				this.printErrorsAndTerminate(state);
+
+				Map<String, Integer> resolvedSymbols = state.scopeMgr.getResolvedSymbols().stream()
+						.collect(Collectors.toMap(res -> res.name, res -> res.address, (entry1, entry2) -> entry2));
+				return new KickAssemblerResult(result.toByteArray(), resolvedSymbols);
+			} catch (AsmErrorException e) {
+				IDiagnostic asmError = e.getError();
+				asmError.setCallStack(state.callStack);
+				System.err.println(OneLineErrorPrinter.instance.printError(asmError, state));
+				throw new AsmErrorException(asmError, false);
+			} catch (Exception e) {
+				throw new RuntimeException("Internal Error!", e);
+			}
 		}
 	}
 
