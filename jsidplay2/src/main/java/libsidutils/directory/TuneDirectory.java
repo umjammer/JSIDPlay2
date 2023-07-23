b67242f55f82d8858d6356091fa43b4ef9104348
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2023-07-23 08:27:42 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4913 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/libsidutils/directory/TuneDirectory.java
+++ jsidplay2/src/main/java/libsidutils/directory/TuneDirectory.java
@@ -1,6 +1,5 @@
 package libsidutils.directory;
 
-import static java.nio.charset.StandardCharsets.ISO_8859_1;
 import static libsidutils.Petscii.iso88591ToPetscii;
 import static libsidutils.directory.DirEntry.FILETYPE_NONE;
 
@@ -50,98 +49,61 @@ public class TuneDirectory extends Directory {
 		if (descriptionIt.hasNext()) {
 			released = descriptionIt.next();
 		}
-		super.title = iso88591ToPetscii(title.toUpperCase(Locale.US), 16);
-		// Directory id: start song '/' song count
-		id = (String.valueOf(info.getStartSong()) + "/" + String.valueOf(Math.max(1, info.getSongs())))
-				.getBytes(ISO_8859_1);
+		super.title = toTitle(title);
+		id = toId(info.getStartSong(), info.getSongs());
 
 		if (author != null) {
-			dirEntries.add(
-					new DirEntry(0, iso88591ToPetscii(author.toUpperCase(Locale.US), MAXLEN_FILENAME), FILETYPE_NONE));
+			dirEntries.add(toDirEntry(author));
 		}
 		if (released != null) {
-			dirEntries.add(new DirEntry(0, iso88591ToPetscii(released.toUpperCase(Locale.US), MAXLEN_FILENAME),
-					FILETYPE_NONE));
+			dirEntries.add(toDirEntry(released));
 		}
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("FORMAT" + "=" + tune.getClass().getSimpleName().toUpperCase(Locale.US),
-						MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries
-				.add(new DirEntry(0,
-						iso88591ToPetscii("PLAYERID" + "="
-								+ tune.identify().stream().collect(Collectors.joining(",")).toUpperCase(Locale.US),
-								MAXLEN_FILENAME),
-						FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("CLOCKFREQ" + "=" + String.valueOf(info.getClockSpeed()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("SPEED" + "=" + String.valueOf(tune.getSongSpeed(1)), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("SIDMODEL1" + "=" + String.valueOf(info.getSIDModel(0)), MAXLEN_FILENAME),
-				FILETYPE_NONE));
+		String playerId = tune.identify().stream().collect(Collectors.joining(","));
+		if (playerId != null && playerId.length() > 0) {
+			dirEntries.add(toDirEntry(playerId));
+		}
+		dirEntries.add(toDirEntry("FORMAT=" + tune.getClass().getSimpleName()));
+		dirEntries.add(toDirEntry("CLOCKFREQ=" + info.getClockSpeed()));
+		dirEntries.add(toDirEntry("SPEED=" + tune.getSongSpeed(1)));
+		dirEntries.add(toDirEntry("SIDMODEL1=" + info.getSIDModel(0)));
 		if (info.getSIDModel(1) != Model.UNKNOWN) {
-			dirEntries.add(new DirEntry(0,
-					iso88591ToPetscii("SIDMODEL2" + "=" + String.valueOf(info.getSIDModel(1)), MAXLEN_FILENAME),
-					FILETYPE_NONE));
+			dirEntries.add(toDirEntry("SIDMODEL2=" + info.getSIDModel(1)));
 		}
 		if (info.getSIDModel(2) != Model.UNKNOWN) {
-			dirEntries.add(new DirEntry(0,
-					iso88591ToPetscii("SIDMODEL3" + "=" + String.valueOf(info.getSIDModel(2)), MAXLEN_FILENAME),
-					FILETYPE_NONE));
+			dirEntries.add(toDirEntry("SIDMODEL3=" + info.getSIDModel(2)));
 		}
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("COMPAT" + "=" + String.valueOf(info.getCompatibility()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("TUNE_LGTH" + "=" + String.valueOf(lengthFnct.getAsDouble()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0, iso88591ToPetscii("AUDIO" + "=" + info.getAudioTypeString(), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("CHIP_BASE1" + "=" + String.valueOf(info.getSIDChipBase(0)), MAXLEN_FILENAME),
-				FILETYPE_NONE));
+		dirEntries.add(toDirEntry("COMPAT=" + info.getCompatibility()));
+		dirEntries.add(toDirEntry("TUNE_LGTH=" + lengthFnct.getAsDouble()));
+		dirEntries.add(toDirEntry("AUDIO=" + info.getAudioTypeString()));
+		dirEntries.add(toDirEntry("CHIP_BASE1=" + info.getSIDChipBase(0)));
 		if (info.getSIDChipBase(1) != 0) {
-			dirEntries.add(new DirEntry(0,
-					iso88591ToPetscii("CHIP_BASE2" + "=" + String.valueOf(info.getSIDChipBase(1)), MAXLEN_FILENAME),
-					FILETYPE_NONE));
+			dirEntries.add(toDirEntry("CHIP_BASE2=" + info.getSIDChipBase(1)));
 		}
 		if (info.getSIDChipBase(2) != 0) {
-			dirEntries.add(new DirEntry(0,
-					iso88591ToPetscii("CHIP_BASE3" + "=" + String.valueOf(info.getSIDChipBase(2)), MAXLEN_FILENAME),
-					FILETYPE_NONE));
+			dirEntries.add(toDirEntry("CHIP_BASE3=" + info.getSIDChipBase(2)));
 		}
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("DRV_ADDR" + "=" + String.valueOf(info.getDeterminedDriverAddr()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("LOAD_ADDR" + "=" + String.valueOf(info.getLoadAddr()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("LOAD_LGTH" + "=" + String.valueOf(info.getC64dataLen()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("INIT_ADDR" + "=" + String.valueOf(info.getInitAddr()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("PLY_ADDR" + "=" + String.valueOf(info.getPlayAddr()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0, iso88591ToPetscii(
-				"FILE_DATE" + "=" + String.valueOf(
-						Instant.ofEpochMilli(tuneFile.lastModified()).atZone(ZoneId.systemDefault()).toLocalDateTime()),
-				MAXLEN_FILENAME), FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("SIZE_KB" + "=" + String.valueOf(tuneFile.length() >> 10), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("SIZE_B" + "=" + String.valueOf(tuneFile.length()), MAXLEN_FILENAME), FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("RELOC_PAGE" + "=" + String.valueOf(info.getRelocStartPage()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
-		dirEntries.add(new DirEntry(0,
-				iso88591ToPetscii("RELOC_PAGES" + "=" + String.valueOf(info.getRelocPages()), MAXLEN_FILENAME),
-				FILETYPE_NONE));
+		dirEntries.add(toDirEntry("DRV_ADDR=" + info.getDeterminedDriverAddr()));
+		dirEntries.add(toDirEntry("LOAD_ADDR=" + info.getLoadAddr()));
+		dirEntries.add(toDirEntry("LOAD_LGTH=" + info.getC64dataLen()));
+		dirEntries.add(toDirEntry("INIT_ADDR=" + info.getInitAddr()));
+		dirEntries.add(toDirEntry("PLY_ADDR=" + info.getPlayAddr()));
+		dirEntries.add(toDirEntry("FILE_DATE="
+				+ Instant.ofEpochMilli(tuneFile.lastModified()).atZone(ZoneId.systemDefault()).toLocalDateTime()));
+		dirEntries.add(toDirEntry("SIZE_KB=" + (tuneFile.length() >> 10)));
+		dirEntries.add(toDirEntry("SIZE_B=" + tuneFile.length()));
+		dirEntries.add(toDirEntry("RELOC_PAGE=" + info.getRelocStartPage()));
+		dirEntries.add(toDirEntry("RELOC_PAGES=" + info.getRelocPages()));
+	}
+
+	private byte[] toTitle(String str) {
+		return iso88591ToPetscii(str.toUpperCase(Locale.US), 16);
+	}
+
+	private byte[] toId(int startSong, int songs) {
+		return iso88591ToPetscii(String.format("%02X/%02X", startSong, songs), 5);
+	}
+
+	private DirEntry toDirEntry(String str) {
+		return new DirEntry(0, iso88591ToPetscii(str.toUpperCase(Locale.US), MAXLEN_FILENAME), FILETYPE_NONE);
 	}
 }
