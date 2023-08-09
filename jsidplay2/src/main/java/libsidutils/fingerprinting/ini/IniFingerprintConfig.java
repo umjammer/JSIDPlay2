3a55f887237d685caa4e05229efbcb0ecf9ba5c3
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2023-08-09 04:29:15 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@4991 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/libsidutils/fingerprinting/ini/IniFingerprintConfig.java
+++ jsidplay2/src/main/java/libsidutils/fingerprinting/ini/IniFingerprintConfig.java
@@ -114,7 +114,12 @@ public class IniFingerprintConfig implements IFingerprintConfig {
 	 * @param iniFile the INI file to backup
 	 */
 	private void createINIBackup(final File iniFile) {
-		iniFile.renameTo(new File(iniFile.getParentFile(), FILE_NAME + ".bak"));
+		File bakFile = new File(iniFile.getParentFile(), FILE_NAME + ".bak");
+		iniFile.renameTo(bakFile);
+
+		int currentVersion = fingerPrintSection.getVersion();
+		System.out.println("Configuration file version=" + currentVersion + " is older than expected version="
+				+ REQUIRED_CONFIG_VERSION + ", a backup has been created: " + bakFile);
 	}
 
 	private void readInternal() {
