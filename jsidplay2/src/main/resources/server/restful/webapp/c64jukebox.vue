33a061c3066298d556d3ce0060de4e46c1141d61
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2023-08-26 05:15:01 +0000

    cleanup2
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@5047 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/resources/server/restful/webapp/c64jukebox.vue
+++ jsidplay2/src/main/resources/server/restful/webapp/c64jukebox.vue
@@ -266,27 +266,6 @@
                         <div class="parent-directory-hint">&larr; {{ $t("parentDirectoryHint") }}</div>
                       </b-list-group-item>
 
-		              <b-button
-		                size="sm"
-		                style="font-size: smaller; padding: 2px 4px; float: right;"
-		                variant="primary"
-		                v-show="directory.filter((entry) => isMusic(entry)).length > 0"
-		                v-on:click="
-		                  directory
-		                    .filter((entry) => isMusic(entry))
-		                    .forEach((entry) =>
-		                      playlist.push({
-		                        filename: entry.filename,
-		                      })
-		                    );
-		                  tabIndex = 5;
-		                  showAudio = true;
-		                "
-		              >
-		                <b-icon-plus> </b-icon-plus>
-		                <span>{{ $t("addAllToPlaylist") }}</span>
-		              </b-button>
-		
                       <b-carousel
                         v-show="directory.filter((entry) => isPicture(entry)).length > 0"
                         id="picture-carousel"
@@ -336,6 +315,27 @@
                           </template>
                         </b-carousel-slide>
                       </b-carousel>
+		              <b-button
+		                size="sm"
+		                style="font-size: smaller; padding: 2px 4px; float: right;"
+		                variant="primary"
+		                v-show="directory.filter((entry) => isMusic(entry)).length > 0"
+		                v-on:click="
+		                  directory
+		                    .filter((entry) => isMusic(entry))
+		                    .forEach((entry) =>
+		                      playlist.push({
+		                        filename: entry.filename,
+		                      })
+		                    );
+		                  tabIndex = 5;
+		                  showAudio = true;
+		                "
+		              >
+		                <b-icon-plus> </b-icon-plus>
+		                <span>{{ $t("addAllToPlaylist") }}</span>
+		              </b-button>
+		
                     </template>
                     <template v-else-if="isDirectory(entry)">
                       <b-list-group-item
