678b6d9c3da0bae79e7a42c7cd8dfddc8d81ba3b
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2023-11-05 11:54:51 +0000

    cleanup
    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@5303 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/server/restful/common/converter/FractionSecondsToMsConverter.java
+++ jsidplay2/src/main/java/server/restful/common/converter/FractionSecondsToMsConverter.java
@@ -17,7 +17,7 @@ public final class FractionSecondsToMsConverter extends BaseConverter<Long> {
 	public Long convert(String value) {
 		try {
 			return Optional.ofNullable(value).filter(v -> !"NaN".equals(v)).map(BigDecimal::new)
-					.map(seconds -> seconds.setScale(3, RoundingMode.DOWN).movePointRight(3).longValue()).orElse(0L);
+					.map(seconds -> seconds.setScale(3, RoundingMode.DOWN).movePointRight(3).longValue()).orElse(null);
 		} catch (NumberFormatException e) {
 			throw new ParameterException(getErrorString(value, "a time in seconds (pattern: ss.SSS)"));
 		}
