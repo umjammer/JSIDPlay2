165729923b06fe49d6dd66389e9443788e5acdb3
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2024-02-29 06:54:52 +0000

    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@5489 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/ui/entities/gamebase/service/GamesService.java
+++ jsidplay2/src/main/java/ui/entities/gamebase/service/GamesService.java
@@ -26,10 +26,18 @@ public class GamesService {
 		Path<String> name = games.get(Games_.name);
 		final Predicate predicate;
 		if (Character.isLetter(firstLetter)) {
-			predicate = cb.like(cb.lower(name), Character.toLowerCase(firstLetter) + "%");
+			predicate = cb.like(name, firstLetter + "%");
 		} else {
 			// first character matches everything EXCEPT letters
-			predicate = cb.or(cb.lessThan(cb.lower(name), "a%"), cb.greaterThan(cb.lower(name), "z%"));
+			predicate = cb.and(cb.not(cb.like(name, "a%")), cb.not(cb.like(name, "b%")), cb.not(cb.like(name, "c%")),
+					cb.not(cb.like(name, "d%")), cb.not(cb.like(name, "e%")), cb.not(cb.like(name, "f%")),
+					cb.not(cb.like(name, "g%")), cb.not(cb.like(name, "h%")), cb.not(cb.like(name, "i%")),
+					cb.not(cb.like(name, "j%")), cb.not(cb.like(name, "k%")), cb.not(cb.like(name, "l%")),
+					cb.not(cb.like(name, "m%")), cb.not(cb.like(name, "n%")), cb.not(cb.like(name, "o%")),
+					cb.not(cb.like(name, "p%")), cb.not(cb.like(name, "q%")), cb.not(cb.like(name, "r%")),
+					cb.not(cb.like(name, "s%")), cb.not(cb.like(name, "t%")), cb.not(cb.like(name, "u%")),
+					cb.not(cb.like(name, "v%")), cb.not(cb.like(name, "w%")), cb.not(cb.like(name, "x%")),
+					cb.not(cb.like(name, "y%")), cb.not(cb.like(name, "z%")));
 		}
 		query.where(predicate).orderBy(cb.asc(name)).select(games);
 		return em.createQuery(query).getResultList();
