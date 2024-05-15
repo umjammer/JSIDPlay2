9fb8001a6ad306bf7a10407b9858e5f05b8de2fb
kenchis <kenchis@05cac4c1-a8e4-46c3-9d4b-80028926d4ef>
2024-05-15 00:04:59 +0000

    git-svn-id: svn://svn.code.sf.net/p/jsidplay2/code/trunk@5740 05cac4c1-a8e4-46c3-9d4b-80028926d4ef

--- jsidplay2/src/main/java/client/teavm/WebAssemblyConfigResolver.java
+++ jsidplay2/src/main/java/client/teavm/WebAssemblyConfigResolver.java
@@ -12,31 +12,76 @@ public class WebAssemblyConfigResolver implements IConfigResolver {
 
 	private static final String C1541_SECTION = "c1541section";
 	
+	@Override
+	public boolean isPalEmulation() {
+		return WebAssemblyConfigResolver.isPalEmulationJS();
+	}
+	
+	@Override
+	public int getSamplingRateAsInt() {
+		return WebAssemblyConfigResolver.getSamplingRateAsIntJS();
+	}
+	
+	@Override
+	public boolean getSamplingMethodResample() {
+		return WebAssemblyConfigResolver.getSamplingMethodResampleJS();
+	}
+	
+	@Override
+	public boolean getReverbBypass() {
+		return WebAssemblyConfigResolver.getReverbBypassJS();
+	}
+	
+	@Override
+	public int getBufferSize() {
+		return WebAssemblyConfigResolver.getBufferSizeJS();
+	}
+
+	@Override
+	public int getAudioBufferSize() {
+		return WebAssemblyConfigResolver.getAudioBufferSizeJS();
+	}
+
+	@Override
+	public boolean getDefaultSidModel8580() {
+		return WebAssemblyConfigResolver.getDefaultSidModel8580JS();
+	}
+	
+	@Override
+	public int getDefaultClockSpeedAsInt() {
+		return WebAssemblyConfigResolver.getDefaultClockSpeedAsIntJS();
+	}
+
+	@Override
+	public boolean isJiffyDosInstalled() {
+		return WebAssemblyConfigResolver.isJiffyDosInstalledJS();
+	}
+	
 	@Import(module = SIDPLAY2_SECTION, name = "getPalEmulation")
-	public native boolean isPalEmulation();
+	public static native boolean isPalEmulationJS();
 
 	@Import(module = AUDIO_SECTION, name = "getSamplingRate")
-	public native int getSamplingRateAsInt();
+	public static native int getSamplingRateAsIntJS();
 
 	@Import(module = AUDIO_SECTION, name = "getSamplingMethodResample")
-	public native boolean getSamplingMethodResample();
+	public static native boolean getSamplingMethodResampleJS();
 
 	@Import(module = AUDIO_SECTION, name = "getReverbBypass")
-	public native boolean getReverbBypass();
+	public static native boolean getReverbBypassJS();
 
 	@Import(module = AUDIO_SECTION, name = "getBufferSize")
-	public native int getBufferSize();
+	public static native int getBufferSizeJS();
 
 	@Import(module = AUDIO_SECTION, name = "getAudioBufferSize")
-	public native int getAudioBufferSize();
+	public static native int getAudioBufferSizeJS();
 
 	@Import(module = EMULATION_SECTION, name = "getDefaultSidModel8580")
-	public native boolean getDefaultSidModel8580();
+	public static native boolean getDefaultSidModel8580JS();
 
 	@Import(module = EMULATION_SECTION, name = "getDefaultClockSpeed")
-	public native int getDefaultClockSpeedAsInt();
+	public static native int getDefaultClockSpeedAsIntJS();
 
 	@Import(module = C1541_SECTION, name = "isJiffyDosInstalled")
-	public native boolean isJiffyDosInstalled();
+	public static native boolean isJiffyDosInstalledJS();
 	
 }
